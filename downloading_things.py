cbc97a8c22ab531541028f2f5a96c32ab5b77cae
moura <moura3950@gmail.com>
2020-07-08 16:59:54 -0300

    Giving a more professional look

--- downloading_things.py
+++ downloading_things.py
@@ -10,10 +10,16 @@ def download_it(url, filename: str):
         response.raise_for_status()
     except Exception:
         print("The requested url does not exist.")
-
     with open(filename, 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == '__main__':
-    download_it(*argv[1:])
+    try:
+        download_it(*argv[1:])
+    except TypeError:
+        print("\n\nThe download function works with only 2 arguments:\n"
+              "\turl: The url you want to request a file.\n"
+              "\tfilename: a str argument that accepts the file name (obviously).\n\n"
+              "Syntax:\n"
+              f"\tpython downloading_things.py <https:\\www.Something_cOoL>.com> yay.anything")
