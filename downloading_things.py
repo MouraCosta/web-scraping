6f4c6e7cc1aa9ff83b05a923ad27fd2f5fa13dd5
moura <moura3950@gmail.com>
2020-07-08 16:28:52 -0300

    Make an functional script

--- downloading_things.py
+++ downloading_things.py
@@ -1,8 +1,15 @@
 import requests
+from sys import argv
 
-response = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-response.raise_for_status()
 
-with open('newFile.txt', 'wb') as file:
-    for chunk in response.iter_content(100000):
-        file.write(chunk)
+def download_it(url, filename: str):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        for chunk in response.iter_content(100000):
+            file.write(chunk)
+
+
+if __name__ == '__main__':
+    download_it(*argv[1:])
