cd5af077b123eef91a5c74eda59e4205d4b8278f
moura <moura3950@gmail.com>
2020-07-11 18:12:46 -0300

    Using threads

--- training_requests.py
+++ training_requests.py
@@ -1,11 +1,21 @@
 import requests
+from concurrent.futures.thread import ThreadPoolExecutor
+
+def make_request(url):
+    req = requests.get(url)
+    print(req.status_code)
+    print(req.url)
+    print()
 
 
 def main():
-    request = requests.get('https://github.com/timeline.json')
-    timeline_dict = request.json()
-
-    print(timeline_dict)
+    urls = [
+        'https://www.youtube.com/watch?v=rZGi1SCj1fU&t=31s',
+        'https://www.debian.org/',
+        'https://docs.oracle.com/javase/tutorial/java/index.html'
+    ]
+    with ThreadPoolExecutor() as executor:
+        executor.map(make_request, urls)
 
 
 if __name__ == '__main__':
