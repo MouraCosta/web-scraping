db3fe6f83ebe75b90e27be3132b1ea51f8bc280f
moura <moura3950@gmail.com>
2020-07-17 16:06:24 -0300

    A more pythonic style

--- lucky.py
+++ lucky.py
@@ -1,24 +1,12 @@
 import sys
 import webbrowser
-import requests
-from bs4 import BeautifulSoup
-
-
-def fil_url(x):
-    cond = 'url' in x.get('href') and '/search' not in x.get('href')
-    return cond
+from googlesearch import search
 
 
 def lucky(query):
-    url = f'https://www.google.com.br/search?q={query}'
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    urls = filter(fil_url, soup.select('div > a[href]'))
-    urls = map(lambda y: y.get('href'), urls)
-    urls = list(map(lambda z: z[z.index('=')+1:], urls))
-
-    for index in urls:
-        print(webbrowser.open(index))
+    urls = search(query, lang='pt-br', stop=5)
+    for url in urls:
+        webbrowser.open(url)
 
 
 if __name__ == "__main__":
